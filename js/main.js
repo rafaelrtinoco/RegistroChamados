de2bc755ba71c6f9c3f21b3bb24fe1da9a91a184
rafabookaa <rafabookaa@gmail.com>
2023-09-09 10:09:15 -0300

    adicionando chamados na tabela

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@ botaoRegistra.addEventListener("click", (event) => {
 
   let form = document.querySelector(".form-adiciona");
 
-  let chamado = obtemDadosChamado (form)
+  let chamado = obtemDadosChamado (form);  
 
-  console.log(chamado)
+  adicionaChamadoNaTabela(chamado);
+  
+  form.reset();
 })
 
+function adicionaChamadoNaTabela (chamado) {
+  let chamadoTr = montaTr(chamado);
+  let tabela = document.querySelector("#tabela-chamados");
+  tabela.appendChild(chamadoTr);
+}
 
 
 function obtemDadosChamado(form) {
@@ -17,7 +24,31 @@ function obtemDadosChamado(form) {
     siglaFilial: form.sigla.value,
     operadora: form.operadora.value,
     sla: form.sla.value,
-    descricao: form.descricao.value
+    descricao: form.descricao.value,
+    chamado: form.chamado.value
   }
   return chamado;
+  
+}
+
+function montaTr (chamado) {
+  let chamadoTr = document.createElement("tr");
+  chamadoTr.classList.add("tr-cabecalho");
+
+  chamadoTr.appendChild(criaTd(chamado.siglaFilial, "info-sigla")); 
+  chamadoTr.appendChild(criaTd(chamado.operadora, "info-operadora"));
+  chamadoTr.appendChild(criaTd(chamado.sla, "info-sla"));
+  chamadoTr.appendChild(criaTd(chamado.descricao, "info-descricao"));
+  chamadoTr.appendChild(criaTd(chamado.chamado, "info-chamado"));
+
+  return chamadoTr;
+
+}
+
+function criaTd (dadoChamado, classe) {
+  let td = document.createElement("td");
+  td.textContent = dadoChamado;
+  td.classList.add(classe);
+
+  return td;
 }
